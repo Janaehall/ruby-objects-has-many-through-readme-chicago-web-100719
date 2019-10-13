e96941c7e6e9ebc4b156f037c97e158f46c1b723
Janae Hall <janaeelisa@gmail.com>
2019-10-13 06:16:55 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,10 @@
 class Meal
   attr_reader :waiter, :customer, :total, :tip
+
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+  end
 end
